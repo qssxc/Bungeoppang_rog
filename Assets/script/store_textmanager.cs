0d01b3140a7188fac86e988b799fe8c53edb1ae4
qssxc <96112943+qssxc@users.noreply.github.com>
2024-04-15 19:37:50 +0900

    gp

---
+++ Assets/script/store_textmanager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class store_textmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Text text;
+}
